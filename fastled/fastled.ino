71242162254695032b01867ce27ee653669dc6fc
David Guttman <david@davidguttman.com>
2015-09-06 21:30:12 -0700

    moved to patterns

--- fastled/fastled.ino
+++ fastled/fastled.ino
@@ -1,27 +1,32 @@
 #include "FastLED.h"
+#include <stdint.h>
+
 
 // How many leds in your strip?
-#define NUM_LEDS 40
+#define NUM_LEDS 17
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-#define DATA_PIN 2
-#define CLOCK_PIN 3
+#define DATA_PIN 11
+#define CLOCK_PIN 13
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
 void setup() {
   Serial.begin(115200);
-  FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
 }
 
 void loop() {
   while (Serial.available()) {
-    Serial.readBytes( (char*)leds, NUM_LEDS * 3);
+    Serial.readBytes( (char*)leds, NUM_LEDS * 3);   
+    
     FastLED.show();
   }
+  
+  FastLED.show();
 
   delay(100);
 }
